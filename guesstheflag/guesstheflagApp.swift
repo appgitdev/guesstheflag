55b1bfc9ef27d1ca92500da3348f8d80d63da852
ferdiangunawan <ferdian@majoo.id>
2022-10-06 08:32:43 +0700

    Initial Commit

---
+++ guesstheflag/guesstheflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guesstheflagApp.swift
+//  guesstheflag
+//
+//  Created by Ferdian Gunawan on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct guesstheflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
